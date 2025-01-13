dcccd92a3a3ddf8b481789b4a59ebb10d618d650
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2025-01-13 17:40:16 +0100

    Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,18 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        //Create an array of double with array length of the length passed in as argument
+        //Iterate for "length" times using a for-loop
+        //For each loop insert the multiples of "number" of argument passed in
+        //Return the array
+
+        double[] numbers = new double[length];
+        for (int i = 0; i < length; i++)
+        {
+            numbers[i] = number * (i + 1);
+        }
+
+        return numbers; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +40,27 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Ensure the supplied "amount" is not less than data.Count and not zero, and data list is not null
+        if (data !== null && amount < data.Count && amount < 1)
+        {
+            //Get the length of the list.
+            int difference = data.Count - amount;
+            //Create a temporary list
+            List<int> list = new List<int>(amount);
+            for (int i = 0; i < amount; i++)
+            {
+                list.Add(data[difference]);
+                data.RemoveAt(difference);
+            }
+            //Copy the temporary list to the newly created list2
+            list<int> list2 = new List<int>(list);
+            //Merge the modified data list to list2
+            list2.AddRange(data);
+        }
+        else
+        {
+            writeLine("Enter Amount less than " + ${ data.Count});
+        }
     }
 }
