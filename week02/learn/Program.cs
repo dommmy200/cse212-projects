2e7d3fa249adefbbb106a7ad01f35c3aa73a538c
dommmy200 <121255352+dommmy200@users.noreply.github.com>
2025-01-15 06:38:48 +0100

    SimpleQueue demonstrated

--- week02/learn/Program.cs
+++ week02/learn/Program.cs
@@ -1,8 +1,8 @@
-﻿Console.WriteLine("\n======================\nSimple Queue\n======================");
-SimpleQueue.Run();
+﻿// Console.WriteLine("\n======================\nSimple Queue\n======================");
+// SimpleQueue.Run();
 
 // Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nSimple Queue\n======================");
-// SimpleQueueSolution.Run();
+Console.WriteLine("\n======================\nSimple Queue\n======================");
+SimpleQueueSolution.Run();
 // SimpleStack.Run();
 // SimpleStackSolution.Run();
